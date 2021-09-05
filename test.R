4b3e7657a9b2b08856cb2851db66e03e23d99ac4
Marketos-Damigos <anubis.coder@gmail.com>
2021-09-05 17:44:24 +0000

    Implementation of Euclidean algo in euclidean()

--- test.R
+++ test.R
@@ -1,3 +1,9 @@
-print("test")
-print("test2")
-print("test3")
+euclidean <-function(x,y){
+  if (x == 0){
+    return(y)
+  }
+  return(euclidean(y %% x, x))
+}
+
+euclidean(123612, 13892347912)
+euclidean(100, 1000)
