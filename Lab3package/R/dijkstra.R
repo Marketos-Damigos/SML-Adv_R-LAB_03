21b9f45130f7e45db4b02dcf8977607b6b160cbd
Marketos-Damigos <anubis.coder@gmail.com>
2021-09-12 10:02:12 +0000

    Small Corrections

--- Lab3package/R/dijkstra.R
+++ Lab3package/R/dijkstra.R
@@ -1,6 +1,8 @@
 #' Dijkstra Algorithm
 #' 
 #' @author Marketos Damgios, Chrystoforos Spyretos
+#' @description Implemantation of Dijkstra's Algorithm in R.
+#' @usage dijkstra(graph, init_node)
 #' @param graph A DataFrame.
 #' @param init_node An Int.
 #' @return Returns the shortest path to every other node from the starting node as a vector.
@@ -11,6 +13,17 @@
 
 
 dijkstra <-function(graph, init_node) {
+  
+  if(!is.numeric(init_node) || !is.data.frame(graph)){
+    stop("The input variables does not have correct str. Please check documentation with ?dijkstra")
+  }
+  if(!(init_node %in% unique(graph$v1))){
+    stop("The init_node value does not exist in the graph. Please check documentation with ?dijkstra")
+  }
+  if(ncol(graph)!= 3 || !(all(colnames(graph) == c("v1", "v2" , "w")))){
+    stop("The input graph does not have correct dimmensions or collumn names. Please check documentation with ?dijkstra")
+  }
+  
   col_names = c("index", "dist", "flag")
   df = data.frame(unique(graph$v1),Inf,0)
   colnames(df) = col_names
